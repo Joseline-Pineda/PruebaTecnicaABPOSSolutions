cc1ad72d9eb46441851a54b7330460062e296a95
Ale <joseline.pineda@catolica.edu.sv>
2024-05-08 16:44:18 -0600

    Agregando nueva propiedad en la entidad "Negocio"

---
+++ PruebaTecnicaABPOSSolutions/Data/Migrations/20240508224146_AgregandoPropEnNegocio.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PruebaTecnicaABPOSSolutions.Data.Migrations
+{
+    public partial class AgregandoPropEnNegocio : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Negocios",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Negocios_UserId",
+                table: "Negocios",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Negocios_AspNetUsers_UserId",
+                table: "Negocios",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Negocios_AspNetUsers_UserId",
+                table: "Negocios");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Negocios_UserId",
+                table: "Negocios");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Negocios");
+        }
+    }
+}
