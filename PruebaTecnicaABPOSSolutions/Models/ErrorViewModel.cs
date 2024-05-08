276c7ab8969c034fe698c70dd77df2ee55d2cfd9
Ale <joseline.pineda@catolica.edu.sv>
2024-05-08 15:59:43 -0600

    Agregar archivos de proyecto.

---
+++ PruebaTecnicaABPOSSolutions/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PruebaTecnicaABPOSSolutions.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
