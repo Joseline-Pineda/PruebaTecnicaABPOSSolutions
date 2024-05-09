90033b2a39854be73adda9f81284ee61862d2e2c
Ale <ale23defebrero.2000@gmail.com>
2024-05-09 11:09:37 -0600

    Agregando nueva migracion...

--- PruebaTecnicaABPOSSolutions/Data/ApplicationDbContext.cs
+++ PruebaTecnicaABPOSSolutions/Data/ApplicationDbContext.cs
@@ -4,13 +4,12 @@ using PruebaTecnicaABPOSSolutions.Models;
 
 namespace PruebaTecnicaABPOSSolutions.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<User>
     {
+        public DbSet<Negocio>? Negocios { get; set; }
+        public DbSet<Categoria>? Categorias { get; set; }
+        public DbSet<Menu>? Menus { get; set; }
 
-        public DbSet<User> Users { get; set; }
-        public DbSet<Negocio> Negocios { get; set; }
-        public DbSet<Categoria> Categorias { get; set; }
-        public DbSet<Menu> Menus { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
