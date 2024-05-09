66a5e3359ad1d16e35eaf151f4db63bad6c82cbc
Ale <joseline.pineda@catolica.edu.sv>
2024-05-08 18:20:49 -0600

    Agregando login y register

---
+++ PruebaTecnicaABPOSSolutions/Models/InputModelUser.cs
@@ -0,0 +1,10 @@
+﻿using static Microsoft.AspNetCore.Identity.UI.V4.Pages.Account.Internal.ExternalLoginModel;
+
+namespace PruebaTecnicaABPOSSolutions.Models
+{
+    public class InputModelUser: InputModel
+    {
+        public string? Nombres { get; set; }
+        public string? Apellidos { get; set; }
+    }
+}
